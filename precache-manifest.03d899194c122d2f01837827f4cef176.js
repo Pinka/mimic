8310171bf8b6b351b88340a48403fb76b75690c2
martinspenkevics <martins.penkevics@emergn.com>
2020-01-07 00:10:59 +0200

    Updates

---
+++ precache-manifest.03d899194c122d2f01837827f4cef176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db00dddea67b877abcdfe9b2f9b33ad5",
+    "url": "/Pinka/mimic/index.html"
+  },
+  {
+    "revision": "8c93b4fd4edd9445ea46",
+    "url": "/Pinka/mimic/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ee20c65dbca4761873e",
+    "url": "/Pinka/mimic/static/js/2.176439c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pinka/mimic/static/js/2.176439c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c93b4fd4edd9445ea46",
+    "url": "/Pinka/mimic/static/js/main.ddd6eb80.chunk.js"
+  },
+  {
+    "revision": "d7c29140e38e98a1fa19",
+    "url": "/Pinka/mimic/static/js/runtime-main.3b558eba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pinka/mimic/static/media/logo.5d5d9eef.svg"
+  }
+]);
