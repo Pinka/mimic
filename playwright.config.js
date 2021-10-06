1e0a378ecf6da01025a8b70169973a4e247eb144
pinka <mpenkevics@gmail.com>
2021-10-07 00:07:25 +0300

    test server

--- playwright.config.js
+++ playwright.config.js
@@ -1,28 +1,36 @@
-const path = require('path');
+const path = require("path");
 
-const testDir = 'tests';
-const outputDir = 'test-results';
+const testDir = "tests";
+const outputDir = "test-results";
 
 const config = {
-    testDir,
-    outputDir,
-    forbidOnly: !!process.env.CI,
-    preserveOutput: process.env.CI ? 'failures-only' : 'always',
-    retries: process.env.CI ? 1 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: process.env.CI ? [
-        [ 'dot' ],
-        [ 'json', { outputFile: path.join(testDir, outputDir, 'report.json') } ],
-      ] : 'line',
-    use: {
-        headless: false,
-        viewport: {
-            width: 1280,
-            height: 720
-        },
-        ignoreHTTPSErrors: true,
-        video: 'on-first-retry',
+  testDir,
+  outputDir,
+  forbidOnly: !!process.env.CI,
+  preserveOutput: process.env.CI ? "failures-only" : "always",
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI
+    ? [
+        ["dot"],
+        ["json", { outputFile: path.join(testDir, outputDir, "report.json") }],
+      ]
+    : "line",
+  use: {
+    headless: false,
+    viewport: {
+      width: 1280,
+      height: 720,
     },
+    ignoreHTTPSErrors: true,
+    video: "on-first-retry",
+  },
+  webServer: {
+    command: "npm run start",
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 };
 
 module.exports = config;
